363b9fb0ee32d89107813be7199119f55f63485f
sli <sli@movoto.com>
2016-04-15 18:38:31 +0800

    more updates.

---
+++ src/zhujianbu/htmlParser.js
@@ -0,0 +1,35 @@
+/**
+ * Created by steven on 4/15/16.
+ */
+/*
+ Usually these methods are running in browser side.
+ */
+
+module.exports.parseCurPageSize = function () {
+    var matches = this.fetchText('#labPageInfo').match(/第(\d+)页/);
+    return matches ? parseInt(matches[1]) : null;
+};
+
+module.exports.parseMaxPageSize = function () {
+    var matches = this.fetchText('#labPageInfo').match(/共(\d+)页/);
+    return matches ? parseInt(matches[1]) : null;
+};
+
+module.exports.parseTH = function () {
+    return this.evaluate(function () {
+        return __utils__.findAll('#dgEnterpriseList tr:first-child td').map(function (td) {
+            return td.innerText;
+        })
+    });
+};
+
+module.exports.parseTR = function () {
+    return this.evaluate(function () {
+        var trs = __utils__.findAll('#dgEnterpriseList tr:not(:first-child)');
+        return trs.map(function (tr) {
+            return Array.prototype.slice.call(tr.querySelectorAll('td')).map(function (td) {
+                return td.innerText;
+            })
+        });
+    });
+};
--- src/zhujianbu/type-1.js
+++ src/zhujianbu/type-1.js
@@ -3,32 +3,65 @@
  */
 'use strict';
 
+var fs = require('fs');
+var csv = require('to-csv');
 var casper = require('casper').create(
     {
         verbose: true,
         logLevel: 'debug'
     }
 );
-var common = require('./common');
+var htmlParser = require('./htmlParser');
 
-casper.start('http://219.142.101.79/regist/wfRegister.aspx?type=1');
+var provinceName = '青海';
+var maxPageSize = 0;
 
-casper.then(function () {
-    this.click('input[type=submit][name=Button1]');
-});
-casper.then(function () {
-    this.log(common.getMaxPageSize(this.fetchText('#labPageInfo')), 'info');
-});
-casper.then(function () {
-    this.click('#LinkButton3');
-});
-casper.then(function () {
-    this.log(this.fetchText('#labPageInfo'), 'info');
-});
-casper.then(function () {
-    this.log('Done', 'info');
-    this.exit();
-});
+casper
+    .start('http://219.142.101.79/regist/wfRegister.aspx?type=1')
+    .then(function setProvince() {
+        var provinceValue = this.evaluate(function (name) {
+            var options = __utils__.findAll('select#ddlManageDep option');
+            for (var i = 0; i < options.length; i++) {
+                if (options[i].innerText === name) {
+                    document.querySelector('select#ddlManageDep').value = options[i].value;
+                    return options[i].value
+                }
+            }
+        }, provinceName);
+        if (!provinceValue) {
+            this.log(value + ' Not Found!', 'error');
+            casper.exit(0)
+        }
+    })
+    .thenClick('input[type=submit][name=Button1]', function search() {// click search button
+        maxPageSize = htmlParser.parseMaxPageSize.call(this);
+        var header = htmlParser.parseTH.call(this);
+        output([header], 'w');
+        this.log(JSON.stringify(header), 'debug');
+        var curPageSize = htmlParser.parseCurPageSize.call(this);
+        this.log('Page: ' + curPageSize + '/' + maxPageSize, 'info');
+    })
+    .then(function () {
+            this
+                .repeat(maxPageSize, function next() {// click next page button.
+                    var curPageSize = htmlParser.parseCurPageSize.call(this);
+                    this.log('Page: ' + curPageSize + '/' + maxPageSize, 'info');
+                    var rows = htmlParser.parseTR.call(this);
+                    this.log(JSON.stringify(rows), 'debug');
+                    output(rows);
+                    this.click('#LinkButton3');
+                })
+                .then(function done() {
+                    this.log('DONE', 'info');
+                })
+        }
+    )
+;
 
 casper.run();
 
+function output(lineArr, mode) {
+    if (casper.options.logLevel === 'debug') return;
+    mode = mode || 'a';
+    fs.write('/home/steven/MyProjects/Github/Crawlers/src/zhujianbu/outputs/all.txt', csv(lineArr, {headers: false}), mode);
+}
